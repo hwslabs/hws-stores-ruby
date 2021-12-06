bdb19d9f190ae2ea4ef1705a3414cefb3397a148
Karthick <karthvin@hypto.in>
2021-12-06 07:53:39 +0530

    Expose owner APIs

--- lib/hws-stores.rb
+++ lib/hws-stores.rb
@@ -7,7 +7,7 @@ module Hws
 
       if allowed_input.key?('data')
         if allowed_input['data'].is_a?(Numeric)
-          allowed_input['value'] == allowed_input.delete('data')
+          allowed_input['value'] = allowed_input.delete('data')
         else
           allowed_input['data'] = allowed_input['data'].with_indifferent_access
         end
@@ -17,16 +17,33 @@ module Hws
       store.try(:id)
     end
 
-    def self.update_store(store_id, data)
-      data = data.with_indifferent_access
+    def self.update_store(store_id:, data: nil, tags: nil)
       store = ::Hws::Stores::Models::Store.find(store_id)
       return false if store.nil?
 
-      if data.is_a?(Numeric)
-        store.update!(value: data, data: nil)
-      else
-        store.update!(value: nil, data: data)
+      update_hash = {}
+
+      update_hash[data.is_a?(Numeric) ? 'value' : 'data'] = data if data.present?
+      update_hash['tags'] = store.tags.merge(tags) if tags.present?
+
+      store.update!(update_hash)
+    end
+
+    def self.get_store(store_id)
+      store = ::Hws::Stores::Models::Store.find(store_id)
+      { id: store.id, name: store.name, description: store.description,
+        data: store.value || store.data, tags: store.tags }.with_indifferent_access
+    end
+
+    def self.increment(store_id, value)
+      store = ::Hws::Stores::Models::Store.find(store_id)
+      return false if store.nil?
+
+      if store.data.present?
+        raise 'Operation allowed only on a value store.'
       end
+
+      store.increment!(:value, value)
     end
 
     def self.delete_store(store_id)
@@ -34,6 +51,31 @@ module Hws
       return false if store.nil?
 
       store.delete.present?
+    end
+
+    def create_owner(name:, description: nil, tags: {})
+      owner = ::Hws::Stores::Models::Owner.create(name: name, description: description, tags: tags)
+      owner.try(:id)
+    end
+
+    def update_owner(owner_id:, name: nil, description: nil, tags: {})
+      owner = ::Hws::Stores::Models::Owner.find_by(owner_id)
+      return false if owner.nil?
+
+      update_fields = {}
+      update_fields['name'] = name if name.present?
+      update_fields['description'] = description if description.present?
+      update_fields['tags'] = owner.tags.merge(tags) if tags.present?
+
+      owner.update!(update_fields)
+    end
+
+    def get_owner(owner_id)
+      ::Hws::Stores::Models::Owner.find_by(owner_id).as_json
+    end
+
+    def delete_owner(owner_id)
+      ::Hws::Stores::Models::Owner.find_by(owner_id).try(:delete)
     end
 
     module Models # :nodoc:
